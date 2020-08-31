803d37853741b3e74dc20a932050e6b41b49e853
rinater <56724135+rinater@users.noreply.github.com>
2020-08-31 17:06:05 +0300

    commit # 2

--- task/banking/gui.py
+++ task/banking/gui.py
@@ -4,19 +4,19 @@ from banking import bank
 
 class DesktopInterface:
     def __init__(self):
-        self.b1 = Button(root, text="Создать аккаунт",)
-        self.b2 = Button(root, text="Войти в аккаунт")
-        self.b3 = Button(root, text='Выход', command=exit)
+        self.b1 = Button(root, text="Создать аккаунт",).place(relx=0.1, rely=0.1)
+        self.b2 = Button(root, text="Войти в аккаунт").place(relx=0.1, rely=0.2)
+        self.b3 = Button(root, text='Выход', command=self.close).place(relx=0.9, rely=0.9)
         self.l1 = Label(root, width=50)
         self.l2 = Label(root)
-        self.b1.pack()
-        self.b2.pack()
-        self.b3.pack()
+        # self.b1.pack()
+        #self.b2.pack()
+        #self.b3.pack()
         self.l1.pack()
         self.login_field = Entry(root, width=50)
         self.pin_field = Entry(root, width=50)
-        self.b1.bind('<Button-1>', self.print_generated_card)
-        self.b2.bind('<Button-1>', self.login_try)
+        #self.b1.bind('<Button-1>', self.print_generated_card)
+        #self.b2.bind('<Button-1>', self.login_try)
 
     def print_generated_card(self, event):
         bank.create_account(1)
@@ -31,7 +31,13 @@ class DesktopInterface:
         self.l2.pack()
         self.pin_field.pack()
 
+    def close(self):
+        root.destroy()
+        root.quit()
+
 
 root = Tk()
+root.title('Банкинг')
+root.geometry('640x480')
 first_window = DesktopInterface()
 root.mainloop()
